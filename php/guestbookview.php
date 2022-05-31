d658af326dab21f9bc038c32872c612aed59572f
Prithvi Jamadagni <pritjam@gmail.com>
2022-05-31 17:18:54 +0530

    forgot to add link to stylesheet

--- php/guestbookview.php
+++ php/guestbookview.php
@@ -5,6 +5,7 @@
 	<title>Pritjam</title>
 	<link rel="stylesheet" href="../css/style.css">
 	<link rel="stylesheet" href="../css/navbar.css">
+    <link rel="stylesheet" href="../css/guestbook.css">
 </head>
 
 <body>
