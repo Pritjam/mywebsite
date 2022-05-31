2016e060479b2ee5bb952d83839b10a9087b4d4d
Prithvi Jamadagni <pritjam@gmail.com>
2022-05-31 17:03:28 +0530

    yet more testing

--- php/guestbookadd.php
+++ php/guestbookadd.php
@@ -8,14 +8,14 @@
     <?php
         $book = fopen("guestbook.txt", "a") or die("Unable to open guestbook for appending!");
         $name = $_POST["name"];
-        fwrite(date("Y-m-d", time()));
+        // fwrite(date("Y-m-d", time()));
 
-        fwrite("\t ========");
-        fwrite($book, $name);
-        fwrite($book, "\t");
-        $message = $_POST["message"];
-        fwrite($book, $message);
-        fwrite($book, "\n");
+        // fwrite("\t ========");
+        // fwrite($book, $name);
+        // fwrite($book, "\t");
+        // $message = $_POST["message"];
+        // fwrite($book, $message);
+        // fwrite($book, "\n");
         fclose($book);
 
         header("Location: guestbookview.php");
