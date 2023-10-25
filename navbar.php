c1bb4846c6e04a388b7e37c4d496f2444bf8faf0
Prithvi <pritjam@gmail.com>
2023-10-25 19:01:49 -0400

    testing php on webserver

---
+++ navbar.php
@@ -0,0 +1,6 @@
+<ul class="navbar">
+	<li><a href="index.html">Home</a></li>
+	<li><a href="games.html">My Games</a></li>
+	<li><a href="projects.html">Projects</a></li>
+    <li><a href="index.html">About Me</a></li>
+</ul>
