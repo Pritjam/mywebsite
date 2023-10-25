ac49ad25d9c0b64c30867b46b4cdcc1ff3cc7689
Prithvi <pritjam@gmail.com>
2023-10-25 19:19:50 -0400

    nvm, screw php

--- navbar.php
+++
@@ -1,6 +0,0 @@
-<ul class="navbar">
-	<li><a href="index.html">Home</a></li>
-	<li><a href="games.html">My Games</a></li>
-	<li><a href="projects.html">Projects</a></li>
-    <li><a href="index.html">About Me</a></li>
-</ul>
